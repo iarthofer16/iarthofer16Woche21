b1f86f156745b313916d0d8cd1dc98dce1c7482b
iarthofer16 <isabelle.arthofer@gmail.com>
2019-02-28 10:27:46 +0100

    fill Spinner 1

--- app/src/main/java/net/htlgrieskirchen/pos3/iarthofer16woche21/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/pos3/iarthofer16woche21/MainActivity.java
@@ -1,8 +1,15 @@
 package net.htlgrieskirchen.pos3.iarthofer16woche21;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        filleCategorys();
+        fillSpinner();
     }
 
     public void datePicker(View view) {
+
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        TextView viewById = findViewById(R.id.date_textView);
+                        viewById.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    public void onClick(View view) {
+    }
+
+    private void filleCategorys(){
+
+    }
+
+    private void fillSpinner()
+    {
+        Spinner spinner = findViewById(R.id.ausgabeEingaben_spinner);
+        String[] items = new String []{"Ausgaben", "Einnahmen"};
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
     }
 }
